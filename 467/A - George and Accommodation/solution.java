471325438770f44672178acdb7b5555d92303e47
Jhushiketh Gaddam <gjhushiketh@gmail.com>
2026-08-14 18:21:45 +0530

    Add George and Accommodation [A] from Codeforces

---
+++ 467/A - George and Accommodation/solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class GeorgeAndAccommodation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int count =0;
+        while(t-->0){
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            if(q-p >=2)
+            count++;
+        }
+        System.out.println(count);
+    }
+}
