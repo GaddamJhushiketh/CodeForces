fc8a1c7b719fa347750078a43508bfaa7ac841be
Jhushiketh Gaddam <gjhushiketh@gmail.com>
2026-08-16 10:30:54 +0530

    Add I Wanna Be the Guy [A] from Codeforces

---
+++ 469/A - I Wanna Be the Guy/solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Guy{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean[] passed = new boolean[n + 1];
+        int p = sc.nextInt();
+        for(int i = 0; i < p; i++){
+            passed[sc.nextInt()] = true;
+        }
+        int q = sc.nextInt();
+        for(int i = 0; i < q; i++){
+            passed[sc.nextInt()] = true;
+        }
+        for(int i = 1; i <= n; i++){
+            if (!passed[i]){
+                System.out.println("Oh, my keyboard!");
+                sc.close();
+                return;
+            }
+        }
+        System.out.println("I become the guy.");
+    }
+}
