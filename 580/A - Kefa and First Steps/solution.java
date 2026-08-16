4f81e8da26c001c5c48bb9b765a5d5d8181e3477
Jhushiketh Gaddam <gjhushiketh@gmail.com>
2026-08-16 10:33:05 +0530

    Add Kefa and First Steps [A] from Codeforces

---
+++ 580/A - Kefa and First Steps/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Kefa{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int ans = 1;
+        int curr = 1;
+        for(int i =1;i<n;i++){
+            if(arr[i] >=arr[i-1]){
+                curr++;
+            }
+            else{
+                curr =1;
+            }
+            ans = Math.max(ans,curr);
+        }
+        System.out.println(ans);
+    }
+}
