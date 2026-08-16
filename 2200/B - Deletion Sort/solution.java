0701724d87496b4248b211ee1096846927c287f1
Jhushiketh Gaddam <gjhushiketh@gmail.com>
2026-08-16 10:32:09 +0530

    Add Deletion Sort [B] from Codeforces

---
+++ 2200/B - Deletion Sort/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Deletion {
+    static boolean isNonDecreasing(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            if (isNonDecreasing(a)) {
+                System.out.println(n);
+            } else {
+                System.out.println(1);
+            }
+        }
+    }
+}
