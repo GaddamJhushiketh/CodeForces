4fd0af9b14e87221e6c0bbc256183f09166641b0
Jhushiketh Gaddam <gjhushiketh@gmail.com>
2026-08-16 10:32:55 +0530

    Add Fox And Snake [A] from Codeforces

---
+++ 510/A - Fox And Snake/solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class FoxAndSnake {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        boolean flag = true;
+        for(int i =1;i<=n;i++){
+            if(i%2!=0){
+                for(int j =0;j<m;j++){
+                    System.out.print("#");
+                }
+            }
+            else{
+                if(flag){
+                    for(int j=0;j<m-1;j++){
+                        System.out.print(".");
+                    }
+                    System.out.print("#");
+                    flag = false;
+                }
+                else{
+                    System.out.print("#");
+                    for(int j=0;j<m-1;j++){
+                        System.out.print(".");
+                    }
+                    flag = true;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
